fa527791c77384b62f267c208c975f97ae876cf2
Beatrix Klebe <bea@klebe.blog>
2019-12-02 22:59:38 -0500

    Initial commit: Day 1

---
+++ AdventOfCode2019/Day1.fs
@@ -0,0 +1,18 @@
+﻿module Day1
+
+open System.IO
+
+let modules = Seq.map (fun line -> line |> int) (File.ReadLines("Day1.txt"))
+
+let fuelForMass mass = mass / 3 - 2
+
+let totalFuel moduleMass = 
+    let rec loop remainingMass totalMass = 
+        if remainingMass <= 0 
+        then totalMass
+        else loop (fuelForMass remainingMass) (totalMass + remainingMass)
+    loop (fuelForMass moduleMass) 0
+
+let problem1 : int = Seq.map fuelForMass modules |> Seq.sum
+
+let problem2 : int = Seq.map totalFuel modules |> Seq.sum
---
+++ AdventOfCode2019/Program.fs
@@ -0,0 +1,9 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+[<EntryPoint>]
+let main argv =
+    printfn "Day 1 Problem 1: %i" Day1.problem1
+    printfn "Day 1 Problem 2: %i" Day1.problem2
+    0 // return an integer exit code
